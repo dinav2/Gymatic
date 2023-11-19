ef01bbc5b977cc3e43ac637057af3c688492a7cb
dinav2 <wealthydina@icloud.com>
2023-11-19 14:45:04 -0600

    Changes to middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,16 +8,7 @@ export function middleware(request: NextRequest) {
   const isPublicPath = path === '/login' || path === '/signup'
 
   const token = request.cookies.get('token')?.value || ''
-  let isVerified = false;
-  const value = JSON.parse(atob(request.cookies.get('token').value.split('.')[1]));
-  if (token) {
-    isVerified = value.isVerified || '';
-  }
-
-  if(token && !isVerified) {
-    return NextResponse.redirect(new URL('/verify', request.nextUrl))
-  }
-
+  
   if(isPublicPath && token) {
     return NextResponse.redirect(new URL('/', request.nextUrl))
   }
